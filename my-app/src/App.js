e8b21afde56232db9ed4a98b70bf38e8683fbd66
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-13 23:18:32 +0200

    Breaking App functionalities into Search and List react components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -32,34 +32,47 @@ const list = [
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>
-          {welcome.greeting} {welcome.title}
-        </h1>
-        <h1>
-          {welcome.greeting} {title()}
-        </h1>
-        <label htmlFor="search">Search:</label>
-        <input id="search" type="text"></input>
-      </div>
+    <div>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <Search></Search>
+      <h1>
+        {welcome.greeting} {title()}
+      </h1>
+
       <hr></hr>
-      <div>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>{item.popularLibraries}</span>
-            </li>
-          );
-        })}
-      </div>
-    </>
+      <List />
+    </div>
   );
 }
 export default App;
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>{item.popularLibraries}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text"></input>
+    </div>
+  );
+}
